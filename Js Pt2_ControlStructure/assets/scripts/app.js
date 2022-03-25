71c2509c80cb3af5a690afbecb9abdbffeb60252
Summy Singh <Summy.Singh@cbre.com>
2022-03-25 18:50:00 +0530

    new project added for control structure

--- Js Pt2_ControlStructure/assets/scripts/app.js
+++ Js Pt2_ControlStructure/assets/scripts/app.js
@@ -6,7 +6,7 @@ let logEntries = [];
 
 // Gets input from input field
 function getUserNumberInput() {
-return parseInt(usrInput.value);
+return parseInt(userInput.value);
 }
 
 
@@ -37,42 +37,71 @@ console.log(logEntries);
 
 
 
-function add() {
+function calculateResult(calculationType) {
 const enteredNumber = getUserNumberInput();
+if (
+calculationType !== 'ADD' &&
+calculationType !== 'SUBTRACT' &&
+calculationType !== 'MULTIPLY' &&
+calculationType !== 'DIVIDE' ||
+!enteredNumber
+) {
+return;
+}
+
+
+
+// if (
+// calculationType === 'ADD' ||
+// calculationType === 'SUBTRACT' ||
+// calculationType === 'MULTIPLY' ||
+// calculationType === 'DIVIDE'
+// ) {
+
 const initialResult = currentResult;
+let mathOperator;
+if (calculationType === 'ADD') {
 currentResult += enteredNumber;
-createAndWriteOutput('+', initialResult, enteredNumber);
-writeToLog('ADD', initialResult, enteredNumber, currentResult);
+mathOperator = '+';
+} else if (calculationType === 'SUBTRACT') {
+currentResult -= enteredNumber;
+mathOperator = '-';
+} else if (calculationType === 'MULTIPLY') {
+currentResult *= enteredNumber;
+mathOperator = '*';
+} else if (calculationType === 'DIVIDE') {
+currentResult /= enteredNumber;
+mathOperator = '/';
+}
+
+
+
+createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+
+
+function add() {
+calculateResult('ADD');
 }
 
 
 
 function subtract() {
-const enteredNumber = getUserNumberInput();
-const initialResult = currentResult;
-currentResult -= enteredNumber;
-createAndWriteOutput('-', initialResult, enteredNumber);
-writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+calculateResult('SUBTRACT');
 }
 
 
 
 function multiply() {
-const enteredNumber = getUserNumberInput();
-const initialResult = currentResult;
-currentResult *= enteredNumber;
-createAndWriteOutput('*', initialResult, enteredNumber);
-writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+calculateResult('MULTIPLY');
 }
 
 
 
 function divide() {
-const enteredNumber = getUserNumberInput();
-const initialResult = currentResult;
-currentResult /= enteredNumber;
-createAndWriteOutput('/', initialResult, enteredNumber);
-writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+calculateResult('DIVIDE');
 }
 
 
